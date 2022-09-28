9c5a0a542a715c26b6319f70ffe59bf24488e184
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-09-28 13:36:34 -0300

    criacao do dbcontext via scaffold

---
+++ FuncionariosMVC/FuncionariosMVC/Data/FuncionariosMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FuncionariosMVC.Models;
+
+namespace FuncionariosMVC.Data
+{
+    public class FuncionariosMVCContext : DbContext
+    {
+        public FuncionariosMVCContext (DbContextOptions<FuncionariosMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Funcionario> Funcionario { get; set; } = default!;
+    }
+}
