bf9e9971c26b61ec991e7ea75e3882d08ca1ebfe
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-11-30 17:01:00 -0300

    criaçao departamentos

---
+++ FuncionariosMVC/FuncionariosMVC/Models/Departamentos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuncionariosMVC.Models
+{
+    public class Departamentos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Departamentos()
+        {
+        }
+
+        public Departamentos(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
