dce7218e4dc18a6c42c918af34cb9bd3abb7de17
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-01-07 17:30:50 -0300

    associacao entre departamento e funcionario

--- FuncionariosMVC/FuncionariosMVC/Models/Departamentos.cs
+++ FuncionariosMVC/FuncionariosMVC/Models/Departamentos.cs
@@ -7,7 +7,7 @@ namespace FuncionariosMVC.Models
         [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
-
+        public ICollection<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
         public Departamentos()
         {
         }
